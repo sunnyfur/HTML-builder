e2362db31194f66446bbb08646a1880be613809b
Polina <sunnyfur9@gmail.com>
2022-11-05 19:37:25 +0500

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const { mkdir } = require("node:fs/promises");
+const { copyFile, constants } = require("node:fs/promises");
+const { readdir } = require("fs/promises");
+const fs = require("fs");
+
+const mergeStyles = async () => {
+  const pathFrom = path.join(__dirname, "styles");
+  const pathTo = path.join(__dirname, "project-dist");
+  const fileOutput = fs.createWriteStream(path.join(pathTo, "bundle.css"));
+
+  try {
+    const files = await readdir(pathFrom, {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (file.isFile() && path.extname(file.name) == ".css") {
+        try {
+          const readableStream = fs.createReadStream(
+            path.join(pathFrom, file.name)
+          );
+          readableStream.on("data", (data) => fileOutput.write(data));
+        } catch {
+          console.log("The file could not be read");
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+mergeStyles();
