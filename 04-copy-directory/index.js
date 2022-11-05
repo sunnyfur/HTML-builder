a4b44a2229e03c85d1cb97ddae9a27e17c94c00a
Polina <sunnyfur9@gmail.com>
2022-11-05 20:17:24 +0500

    refactor: add copy from folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,29 +3,33 @@ const { rm, mkdir } = require("node:fs/promises");
 const { copyFile, constants } = require("node:fs/promises");
 const { readdir } = require("fs/promises");
 
-const copyDir = async (pathFrom) => {
-  const pathTo = pathFrom + "-copy";
+const copyDir = async (pathFrom, pathTo) => {
   await rm(pathTo, { force: true, recursive: true });
   await mkdir(pathTo, { recursive: true });
 
-  try {
-    const files = await readdir(pathFrom, {
-      withFileTypes: true,
-    });
-    for (const file of files) {
-      if (file.isFile()) {
-        try {
-          await copyFile(
-            path.join(pathFrom, file.name),
-            path.join(pathTo, file.name)
-          );
-        } catch {
-          console.log("The file could not be copied");
+  const copyFiles = async (pathFrom) => {
+    try {
+      const files = await readdir(pathFrom, {
+        withFileTypes: true,
+      });
+      for (const file of files) {
+        if (file.isFile()) {
+          try {
+            await copyFile(
+              path.join(pathFrom, file.name),
+              path.join(pathTo, file.name)
+            );
+          } catch {
+            console.log("The file could not be copied");
+          }
+        } else {
+          copyFiles(path.join(pathFrom, file.name));
         }
       }
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
+  };
+  copyFiles(pathFrom);
 };
-copyDir(path.join(__dirname, "files"));
+copyDir(path.join(__dirname, "files"), path.join(__dirname, "files-copy"));
